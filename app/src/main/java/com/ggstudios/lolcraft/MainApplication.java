427b1193b682ed74d6fed9ca602dd812058ef610
idunnololz <garyguo9@gmail.com>
2014-11-23 18:07:45 -0800

    Add crashlytics

--- app/src/main/java/com/ggstudios/lolcraft/MainApplication.java
+++ app/src/main/java/com/ggstudios/lolcraft/MainApplication.java
@@ -1,13 +1,16 @@
 package com.ggstudios.lolcraft;
 
+import com.crashlytics.android.Crashlytics;
 import com.ggstudios.utils.DebugLog;
 
 import android.app.Application;
+import io.fabric.sdk.android.Fabric;
 
 public class MainApplication extends Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
+		Fabric.with(this, new Crashlytics());
 		
 		LibraryManager.initInstance(getApplicationContext());
 		SplashFetcher.initInstance(getApplicationContext());
